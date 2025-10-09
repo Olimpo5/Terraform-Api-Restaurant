b6da3ceec2a36f38f86d55f37094ab0cdc84af40
Kevin Tavera <KevTav20>
2025-10-09 15:14:54 -0600

    Correccion DB

--- modules/vm/main.tf
+++ modules/vm/main.tf
@@ -136,7 +136,9 @@ resource "null_resource" "mysql_up" {
     }
     inline = [
       "cd /containers",
+      "aws ecr get-login-password --region us-east-2 | docker login --username AWS --password-stdin 086143043522.dkr.ecr.us-east-2.amazonaws.com",
       "docker compose up -d"
     ]
   }
 }
+
